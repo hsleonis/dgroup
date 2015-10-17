939d21508eef4505e904fd8c53146e98199b0c71
Shahriar <hsleonis@gmail.com>
2015-10-17 14:59:01 +0600

    CMB2 added

--- admin/include.php
+++ admin/include.php
@@ -6,10 +6,11 @@
 */
 
 // Included,in alphabetical order
+require('settings/meta.php');
+require('settings/opt.php');
+require('repeatable/data.php');
 require('theme/enqueue.php');
 require('theme/enterprise.php');
 require('theme/menu.php');
 require('theme/sidebar.php');
 require('theme/support.php');
-require_once('settings/meta.php');
-require_once('settings/opt.php');
