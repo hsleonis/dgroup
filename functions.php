3bd8b87b51546c2211f1181cb9468d831653d193
Shahriar <hsleonis@gmail.com>
2015-10-19 13:04:46 +0600

    Admin menu default collapse

--- functions.php
+++ functions.php
@@ -12,3 +12,11 @@ function ucsmart($text)
    return preg_replace('/([^a-z\']|^)([a-z])/e', '"$1".strtoupper("<b>$2</b>")',
                        strtolower($text));
 }
+
+function admin_menu_js() {
+    echo "<script type='text/javascript' > 
+document.body.className+=' folded';                 
+</script>";
+
+}
+add_action('admin_footer', 'admin_menu_js');
